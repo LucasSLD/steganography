408735c52097d0f0cfe3c0e5faf5ed795a20bb23
LucasSLD <lucassld700@gmail.com>
2024-02-27 14:11:04 +0100

    feat: created model for steganography with quantization error insertion

--- iclr_17_compression/model.py
+++ iclr_17_compression/model.py
@@ -13,7 +13,9 @@ import torch.nn.init as init
 import logging
 from torch.nn.parameter import Parameter
 from models import *
-
+sys.path.append("../quantization_error/")
+from embedding_simulator import Embedding_simulator as es
+from math import log
 
 def save_model(model, iter, name):
     torch.save(model.state_dict(), os.path.join(name, "iter_{}.pth.tar".format(iter)))
@@ -101,6 +103,61 @@ class ImageCompressorSteganography(nn.Module):
             values = torch.multinomial(probabilities,shape[0]*shape[1]*shape[2],replacement=True) - 1
             values = values.reshape((shape[0],shape[1],shape[2])).cuda()
             compressed_feature_renorm[0].add_(values)
+
+        recon_image = self.Decoder(compressed_feature_renorm)
+        # recon_image = prediction + recon_res
+        clipped_recon_image = recon_image.clamp(0., 1.)
+        # distortion
+        mse_loss = torch.mean((recon_image - input_image).pow(2))
+
+        def iclr18_estimate_bits_z(z):
+            prob = self.bitEstimator(z + 0.5) - self.bitEstimator(z - 0.5)
+            total_bits = torch.sum(torch.clamp(-1.0 * torch.log(prob + 1e-10) / math.log(2.0), 0, 50))
+            return total_bits, prob
+
+        total_bits_feature, _ = iclr18_estimate_bits_z(compressed_feature_renorm)
+        im_shape = input_image.size()
+        bpp_feature = total_bits_feature / (batch_size * im_shape[2] * im_shape[3])
+
+        return clipped_recon_image, mse_loss, bpp_feature
+
+def ternary_entropy(p: float) -> float:
+    """
+    Ternary entropy
+    Args:
+        p (float): half insertion rate, probability p of adding 1 and proba p of substracting 1
+
+    Returns:
+        float: ternary entropy for given p
+    """
+    return -2*p*log(p,2) - (1-2*p)*log(1-2*p,2)
+
+class ImageCompressorSteganography_QE(nn.Module): # Image compressor model with steganography performed with quantization error method
+    def __init__(self, p=0.0, out_channel_N=128):
+        super(ImageCompressorSteganography_QE, self).__init__()
+        self.Encoder = Analysis_net_17(out_channel_N=out_channel_N)
+        self.Decoder = Synthesis_net_17(out_channel_N=out_channel_N)
+        self.bitEstimator = BitEstimator(channel=out_channel_N)
+        self.out_channel_N = out_channel_N
+        self.p = p
+
+    def forward(self, input_image, stega=False):
+        quant_noise_feature = torch.zeros(input_image.size(0), self.out_channel_N, input_image.size(2) // 16, input_image.size(3) // 16).cuda()
+        quant_noise_feature = torch.nn.init.uniform_(torch.zeros_like(quant_noise_feature), -0.5, 0.5)
+        feature = self.Encoder(input_image)
+        batch_size = feature.size()[0]
+        compressed_feature_renorm = torch.round(feature)
+
+        if stega:
+            shape = compressed_feature_renorm.shape[1:]
+            message_length = ternary_entropy(self.p)
+            quantization_error = torch.sub(feature,compressed_feature_renorm).flatten()
+            rho_P1 = (1 - 2*quantization_error).cpu().numpy()
+            rho_M1 = (1 + 2*quantization_error).cpu().numpy()
+            p_change_P1, p_change_M1 = es.compute_proba(rho_P1,rho_M1,message_length,shape[0]*shape[1]*shape[2])
+            feature_QE = es.process(compressed_feature_renorm.cpu().numpy().flatten(),p_change_P1,p_change_M1)
+            compressed_feature_renorm[0] = torch.Tensor(feature_QE).reshape(shape[0],shape[1],shape[2])
+            print(torch.mean((compressed_feature_renorm == torch.round(feature)).float()))
 
         recon_image = self.Decoder(compressed_feature_renorm)
         # recon_image = prediction + recon_res
