3cfe86d493b242688103a09b110c8d83fb5c46f3
LucasSLD <lucassld700@gmail.com>
2024-03-02 17:25:37 +0100

    feat: quantization error applied to BossBase dataset

--- quantization_error/simulate_qe.py
+++ quantization_error/simulate_qe.py
@@ -7,9 +7,10 @@ from PIL import Image
 from torchvision import transforms
 import matplotlib.pyplot as plt
 import numpy as np
+sys.path.append("../Tools")
+from my_utils import pgm_to_tensor, plot_tensor, plot_np_array
 
-
-def apply_qe_to_kodak(path_model : str, img_indexes : list[int], plot=False, stega=False, p=.0, plot_precover=False):
+def apply_qe_to_kodak(path_model : str, img_indexes : list[int], plot=False, stega=False, p=.0, plot_precover=False,plot_hist=False):
     with torch.no_grad():
         model = ImageCompressorSteganography_QE(p)
         load_model(model,path_model)
@@ -52,7 +53,7 @@ def apply_qe_to_kodak(path_model : str, img_indexes : list[int], plot=False, ste
                     plot_np_array(cover_img_np,"cover image")
 
                 if stega:
-                    stega_image, _, bpp_stega = net(input,stega=True)
+                    stega_image, _, bpp_stega = net(input,stega=True,plot_hist=plot_hist)
 
                     mse_loss_stega_cover = torch.mean((stega_image - cover_image).pow(2))
                     psnr_stega_cover = 10 * (torch.log(1. / mse_loss_stega_cover) / np.log(10))
@@ -76,3 +77,72 @@ def apply_qe_to_kodak(path_model : str, img_indexes : list[int], plot=False, ste
             return sumBpp_stega.cpu().item(), sumPsnr_stega_cover.cpu().item()
 
         return sumBpp.cpu().item(), sumPsnr.cpu().item()
+
+def network_output_to_numpy(net_out) -> np.ndarray:
+    """Converts output of ImageCompressor networks to numpy arrays
+
+    Args:
+        net_out (Tensor): tensor of an image produced by the ImageCompressor network (or one of its variations). It has the shape (1,channels,height,width)
+
+    Returns:
+        np.ndarray: numpy array of an image with shape (height,width,channels) and values between 0 and 1
+    """
+    assert len(net_out.shape) == 3 or len(net_out.shape) == 4
+    if len(net_out.shape) == 4: net_out = net_out[0] # the first axis is useless when there are 4 dimensions
+    to_pil = transforms.ToPILImage()
+    img = np.array(to_pil(net_out.cpu()))
+    return img
+
+def diff_cover_stego(cover_t, stego_t):
+    """
+    Args:
+        cover_t (Tensor): cover tensor of an image (output of ImageCompressor model)
+        stego_t (Tensor): stego tensor of an image (output of ImageCompressor model)
+
+    Returns:
+        np.ndarray: difference between cover image and stego image
+    """
+    cover_np = network_output_to_numpy(cover_t)
+    stego_np  = network_output_to_numpy(stego_t)
+    return cover_np.astype(int) - stego_np.astype(int)
+
+def apply_qe_to_bossbase_img(
+    precover_folder_path : str,
+    model_path : str,
+    idx: int,
+    p : float = 0.,
+    plot_cover_stego=False,
+    plot_diff=False,
+    figsize=None):
+    """Apply ImageCompressorSteganography_QE to an image from bossbase
+
+    Args:
+        precover_folder_path (str): path to bossbase pgm images folder
+        model_path (str): path to the weights of the model
+        idx (int): number in the name of the pgm file to which we apply the model
+        p (float, optional): half insertion rate (used to get the size of the message). Defaults to 0..
+        plot_cover_stego (bool, optional): _description_. Defaults to False.
+        plot_diff (bool, optional): _description_. Defaults to False.
+
+    Returns:
+        _type_: _description_
+    """
+    with torch.no_grad():
+        model = ImageCompressorSteganography_QE(p)
+        load_model(model,model_path)
+        net = model.cuda()
+        net.eval()
+        
+        img_t = pgm_to_tensor(precover_folder_path + "/" + str(idx) + ".pgm")
+        cover_t, _, _ = net(img_t,stega=False)
+        stego_t, _, _, modification_rate = net(img_t,stega=True,return_modification_rate=True)
+
+        if plot_cover_stego:
+            plot_tensor(cover_t,"cover")
+            plot_tensor(stego_t,"stego")
+        mse_loss = torch.mean((cover_t - stego_t).pow(2))
+        psnr = 10 * (torch.log(1. / mse_loss) / np.log(10))
+        diff = diff_cover_stego(cover_t,stego_t)[:,:,0] # we work with grayscale img so only 1 channel is necessary
+        if plot_diff:
+            plot_np_array(diff,title="cover - stego",colorbar=True,figsize=figsize)
+        return psnr.cpu().numpy(), diff, modification_rate
