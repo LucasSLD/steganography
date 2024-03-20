e1f6ca1b45980488e96e38564a71cc04083abc9c
LucasSLD <lucassld700@gmail.com>
2024-03-20 23:43:44 +0100

    feat: report

---
+++ Documents/final_presentation/clean.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Define the LaTeX document filename (without extension)
+filename="report"
+
+# Remove LaTeX auxiliary files
+rm -f "$filename.aux" "$filename.log" "$filename.out" "$filename.toc" "$filename.lof" "$filename.lot" "$filename.bbl" "$filename.blg" "$filename.synctex.gz" "$filename.gz" "$filename.fdb_latexmk" "$filename.fls" "$filename.auxlock" "$filename.run.xml" "$filename.bcf" "$filename.nav" "$filename.snm"
