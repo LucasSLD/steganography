5ced51ebc11a7d131082a9565869b179eeab1649
LucasSLD <lucassld700@gmail.com>
2024-03-21 22:46:41 +0100

    feat: added parameter to use ImageCompressorSteganography_QE_modified_cost

--- quantization_error/generate_stego_qe.py
+++ quantization_error/generate_stego_qe.py
@@ -1,7 +1,7 @@
 import sys
 sys.path.append("../iclr_17_compression/")
 sys.path.append("../Tools")
-from model import ImageCompressorSteganography_QE
+from model import ImageCompressorSteganography_QE, ImageCompressorSteganography_QE_modified_cost
 from torchvision import transforms
 from PIL import Image
 import numpy as np
@@ -15,7 +15,8 @@ def precover_to_stego(
     precover_folder_path : str, 
     output_path : str,
     model_path : str, 
-    p : float = .0, 
+    p : float = .0,
+    use_modified_cost: bool = False,
     file_id_start : int = 1, 
     file_id_stop : int = -1):
     """
@@ -28,7 +29,11 @@ def precover_to_stego(
         file_id_start (int, optional): Lower bound of the range of images.
         file_id_stop (int, optional): Upper bound of the range of images.
     """
-    model = ImageCompressorSteganography_QE(p)
+    if use_modified_cost:
+        model = ImageCompressorSteganography_QE_modified_cost(p)
+    else:
+        model = ImageCompressorSteganography_QE(p)
+    
     load_model(model,model_path)
     files = os.listdir(precover_folder_path)
     assert p <= .5 # insertion rate cannot be higher than 1
@@ -62,6 +67,7 @@ if __name__ == "__main__":
     parser.add_argument("-e","--end",default=-1,type=int,help="Number of the last image to convert")
     parser.add_argument("-m","--model",required=True,help="Path to the ImageCompressor model to use")
     parser.add_argument("-p","--probability",type=float,default=.0,help="half insertion rate")
+    parser.add_argument("-c","--cost",type=bool,default=False,help="If True, use ImageCompressorSteganography_QE_modified_cost")
 
     args = parser.parse_args()
     if not os.path.exists(args.output):
@@ -70,4 +76,4 @@ if __name__ == "__main__":
         info = f"p = {args.probability}\nH3(p) = {ternary_entropy(args.probability)}"
         with open(args.output + "/" + "_info.txt","w") as f:
             f.write(info)
-    precover_to_stego(args.name, args.output, args.model, args.probability, args.begin, args.end)
+    precover_to_stego(args.name, args.output, args.model, args.probability,args.cost, args.begin, args.end)
