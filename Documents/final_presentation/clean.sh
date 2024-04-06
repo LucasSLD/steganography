6b8124f7bf06859aed979ff50cee4df53bde0ef0
LucasSLD <lucassld700@gmail.com>
2024-04-06 12:01:59 +0200

    feat: clean additional files

--- Documents/final_presentation/clean.sh
+++ Documents/final_presentation/clean.sh
@@ -4,4 +4,4 @@
 filename="report"
 
 # Remove LaTeX auxiliary files
-rm -f "$filename.aux" "$filename.log" "$filename.out" "$filename.toc" "$filename.lof" "$filename.lot" "$filename.bbl" "$filename.blg" "$filename.synctex.gz" "$filename.gz" "$filename.fdb_latexmk" "$filename.fls" "$filename.auxlock" "$filename.run.xml" "$filename.bcf" "$filename.nav" "$filename.snm"
+rm -f "$filename.aux" "$filename.log" "$filename.out" "$filename.toc" "$filename.lof" "$filename.lot" "$filename.bbl" "$filename.blg" "$filename.synctex.gz" "$filename.gz" "$filename.fdb_latexmk" "$filename.fls" "$filename.auxlock" "$filename.run.xml" "$filename.bcf" "$filename.nav" "$filename.snm" "$filename.loa" "$filename.lol" "$filename.tdo" "$filename.bbl-SAVE-ERROR" "$filename.bcf-SAVE-ERROR"
