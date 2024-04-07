76f7275fb65ec3bd19db75040e5aa03848410466
LucasSLD <lucassld700@gmail.com>
2024-04-07 19:51:38 +0200

    feat: clean auxiliary files of the slides too

--- Documents/final_presentation/clean.sh
+++ Documents/final_presentation/clean.sh
@@ -2,6 +2,9 @@
 
 # Define the LaTeX document filename (without extension)
 filename="report"
+slidename="presentation"
 
 # Remove LaTeX auxiliary files
 rm -f "$filename.aux" "$filename.log" "$filename.out" "$filename.toc" "$filename.lof" "$filename.lot" "$filename.bbl" "$filename.blg" "$filename.synctex.gz" "$filename.gz" "$filename.fdb_latexmk" "$filename.fls" "$filename.auxlock" "$filename.run.xml" "$filename.bcf" "$filename.nav" "$filename.snm" "$filename.loa" "$filename.lol" "$filename.tdo" "$filename.bbl-SAVE-ERROR" "$filename.bcf-SAVE-ERROR"
+
+rm -f "$slidename.aux" "$slidename.bcf" "$slidename.fdb_latexmk" "$slidename.fls" "$slidename.log" "$slidename.nav" "$slidename.out" "$slidename.run.xml" "$slidename.snm" "$slidename.synctex.gz" "$slidename.toc" "$slidename.bbl" "$slidename.bbl-SAVE-ERROR" "$slidename.blg"
